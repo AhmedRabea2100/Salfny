d1c2dc11f95c22d50c39588100fdae2faf4c0a1b
OmarMetmwah <omarelbrns29@gmail.com>
2022-12-02 18:46:09 +0200

    CorsMapping Refactoring👷

--- salfny/salfny/src/main/java/com/swe/salfny/controller/Controller.java
+++ salfny/salfny/src/main/java/com/swe/salfny/controller/Controller.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
 public class Controller {
 
     @Autowired
