d73cd5c0b224572339e436d9ca7501317dc7f6c1
AhmedRabea2100 <95850756+AhmedRabea2100@users.noreply.github.com>
2022-12-17 19:38:13 +0200

    edit and save buttons

--- Frontend/src/app/components/userprofile/userprofile.component.ts
+++ Frontend/src/app/components/userprofile/userprofile.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./userprofile.component.css']
 })
 export class UserprofileComponent {
+  ngOnInit(): void {
+  }
 
+  setEnable(){
+    const x = document.getElementById('editable-Email');
+    x?.removeAttribute('disabled');
+    const y = document.getElementById('editable-Phone');
+    y?.removeAttribute('disabled');
+  }
+  editValue(){
+    const x = document.getElementById('editable-Email');
+    x?.setAttribute('disabled', 'true');
+    const y = document.getElementById('editable-Phone');
+    y?.setAttribute('disabled', 'true');
+  }
 }
