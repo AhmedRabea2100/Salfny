c5ca4577a1ff7758c2e4505fe548dd4cea1f0526
MaiAhmedHussein <mayoyaahmed66@gmail.com>
2022-12-18 17:54:51 +0200

    :alien: UnderConstruction DB

--- salfny/salfny/src/main/java/com/swe/salfny/Model/UploRepository.java
+++ salfny/salfny/src/main/java/com/swe/salfny/Model/UploRepository.java
@@ -7,6 +7,6 @@ public interface UploRepository extends JpaRepository<Uplo, Integer> {
     @Query("SELECT u.id FROM User u WHERE u.email = ?1")
     public String findByEmail(String email);
 
-    @Query("SELECT u.password FROM User u WHERE u.email = ?1")
-    public String authenticate(String email);
+    @Query("SELECT MAX(id) FROM Uplo")
+    public long findMaxId();
 }
--- salfny/salfny/src/main/java/com/swe/salfny/controller/UploadController.java
+++ salfny/salfny/src/main/java/com/swe/salfny/controller/UploadController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
+import javax.xml.bind.DatatypeConverter;
+import java.io.*;
 import java.time.LocalDateTime;
 
 @RestController
@@ -19,9 +21,42 @@ public class UploadController {
     //@CrossOrigin
     @RequestMapping("/upload")
     public String upload(@RequestBody Uplo p ) {
-        System.out.println(p.getTitle()+" "+ p.getDescription()+" "+p.getCategory()+" el sora "+p.getPhoto());
+       // System.out.println(p.getTitle()+" "+ p.getDescription()+" "+p.getCategory()+" el sora "+p.getPhoto());
+        p.setPhoto(conv(p.getPhoto(),repo.findMaxId()+1));
         repo.save(p);
         return "back  " ;
 
     }
-}
+
+    public String conv(String photo,long i) {
+        String base64String = photo;
+        String[] strings = base64String.split(",");
+        String extension;
+        switch (strings[0]) {//check image's extension
+            case "data:image/jpeg;base64":
+                extension = "jpeg";
+                break;
+            case "data:image/png;base64":
+                extension = "png";
+                break;
+            default://should write cases for more images types
+                extension = "jpg";
+                break;
+        }
+        //convert base64 string to binary data
+        byte[] data = DatatypeConverter.parseBase64Binary(strings[1]);
+        System.out.println(System.getProperty("user.dir"));
+        String[] s1=System.getProperty("user.dir").split("salfny");
+        String out;
+        if(s1[0].contains("/")) {
+            out = s1[0] + "Frontend/src/assets/images/" + i + "." + extension;//for Linux Users
+        }else{
+            out = s1[0] + "Frontend\\src\\assets\\images\\" + i + "." + extension;//for Windows Users
+        }
+        try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(out))) {
+            outputStream.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return i+"."+extension;
+    }}
